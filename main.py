a96850a0913c84c0e7d81665d6fb35169ca7819d
ruchamy <r0527187861@gmail.com>
2025-03-20 21:11:09 +0200

    csv files

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import pandas as pd
+import os
+
+DATA_DIR = "shopping_data"
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasksDB.db'
@@ -14,6 +21,15 @@ class User(db.Model):
 
 with app.app_context():
     db.create_all()
+
+
+class Purchase:
+    def __init__(self, product_name: str, category: str, quantity: int, price: float, date: str = None):
+        self.product_name = product_name
+        self.category = category
+        self.quantity = quantity
+        self.price = price
+        self.date = date if date else datetime.now().strftime("%Y-%m-%d")  # תאריך ברירת מחדל להיום
 
 
 @app.route('/')
@@ -48,9 +64,40 @@ def register():
     return render_template('register.html')
 
 
-@app.route('/shopping/<user_email>')
+@app.route('/shopping/<user_email>', methods=['GET', 'POST'])
 def shopping(user_email):
-    return render_template('shopping.html', user_email=user_email)
+    file_path = os.path.join(DATA_DIR, f"{user_email}.csv")
+    if request.method == 'POST':
+        new_purchase = pd.DataFrame([{
+            "product": request.form['product'],
+            "category": request.form['category'],
+            "quantity": request.form['quantity'],
+            "price": request.form['price'],
+            "date": request.form['date']
+        }])
+        if os.path.exists(file_path):
+            df = pd.read_csv(file_path)
+            df = pd.concat([df, new_purchase], ignore_index=True)
+        else:
+            df = new_purchase
+        df.to_csv(file_path, index=False)
+    return redirect(url_for('view_purchases', user_email=user_email))
+
+
+@app.route("/view_purchases/<user_email>")
+def view_purchases(user_email):
+    start_date = request.args.get('start_date', '')
+    end_date = request.args.get('end_date', '')
+    file_path = os.path.join(DATA_DIR, f"{user_email}.csv")
+    purchases = []
+    if os.path.exists(file_path):
+        df = pd.read_csv(file_path)
+        if start_date and end_date:
+            df["date"] = pd.to_datetime(df["date"])
+            mask = (df["date"] >= start_date) & (df["date"] <= end_date)
+            df = df[mask]
+        purchases = df.to_dict(orient="records")
+    return render_template('shopping.html', user_email=user_email,purchases=purchases)
 
 
 if __name__ == '__main__':
